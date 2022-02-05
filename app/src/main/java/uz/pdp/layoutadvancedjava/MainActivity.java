2a938256cb9df96d08ea10d21181289e9de6a9c5
avtech <shdiyor041@gmail.com>
2022-02-05 12:36:04 +0500

    ok

--- app/src/main/java/uz/pdp/layoutadvancedjava/MainActivity.java
+++ app/src/main/java/uz/pdp/layoutadvancedjava/MainActivity.java
@@ -2,7 +2,12 @@ package uz.pdp.layoutadvancedjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        TextView textView = findViewById(R.id.textView);
+        String text = "I know just how to #whister, And I know how to cry, I know just where to find the answer";
+
+        Spannable spannable = new SpannableString(text);
+        spannable.setSpan(new ForegroundColorSpan(Color.BLUE), 18, 27, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.setText(spannable);
     }
 }
